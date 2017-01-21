915e49d23075defcb138cc40df942a19e11f3a53
avnerel <avnerelizarov@gmail.com>
2017-01-21 14:17:05 +0200

    added recursive file traversal and checking for each file which tag should ne added. now need to implement adding the tag

---
+++ SinceTag.java
@@ -0,0 +1,21 @@
+package what.ever;
+
+/**
+ * @since now
+ */
+public class NoTags {
+    private int ryan;
+
+    public NoTags() {
+        ryan = 0;
+    }
+
+    public class Dumdum {
+        private int shtut;
+
+        public Dumdum() {
+            shtut = 5;
+        }
+    }
+
+}
